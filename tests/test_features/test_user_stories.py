04f6a5168d782a21153b962dccda6ed209b27551
michaellennox <michaelctlennox@gmail.com>
2015-12-23 17:02:01 +0000

    Adds failing feature test for raising error when station is empty

--- tests/test_features/test_user_stories.py
+++ tests/test_features/test_user_stories.py
@@ -33,3 +33,10 @@ class TestUserStories(unittest.TestCase):
         # I want to see a bike that has been docked
         self.station.dock(self.bike)
         self.assertEqual(self.station.bikes, self.bike)
+
+    def test_docking_station_does_not_release_bike_when_empty(self):
+        # As a member of the public,
+        # So that I am not confused and charged unnecessarily,
+        # I'd like docking stations not to release bikes when there are none available.
+        with self.assertRaisesRegexp(Exception, 'No bikes'):
+            self.station.release_bike()
