10cbcbb18414b70c7b17a0a15d3d6bc5a9dd1d10
michaellennox <michaelctlennox@gmail.com>
2015-12-24 13:30:06 +0000

    Adds garage and van classes and associated test files

---
+++ tests/test_garage.py
@@ -0,0 +1,6 @@
+import unittest
+from mock import Mock
+from app.garage import Garage
+
+class TestGarage(unittest.TestCase):
+    pass
---
+++ tests/test_van.py
@@ -0,0 +1,6 @@
+import unittest
+from mock import Mock
+from app.van import Van
+
+class TestVan(unittest.TestCase):
+    pass
